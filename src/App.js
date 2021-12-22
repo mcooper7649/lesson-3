506b66185a9e78cef79cc2f56d62941ee904b52e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-12-21 22:58:46 -0500

    routerdom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = (props) => {
+  console.log('Hatspage');
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = (props) => {
+  console.log('TopicsList');
+  return (
+    <div>
+      <h1>TopicsList PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsDetail = (props) => {
+  console.log('TopicsDetail');
+  return (
+    <div>
+      <h1>TopicsDetail PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<Hats />} />
+      </Routes>
     </div>
   );
 }
